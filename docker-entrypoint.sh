71ee9dde39894b610223a8557834a10a1c629a2d
raymondmouthaan <raymondmmouthaan@gmail.com>
2019-07-27 09:38:11 +0200

    v0.10.0 for arm32v6, arm64v8 and amd64

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # if command starts with an option, prepend haproxy-exporter binary
 if [ "${1:0:1}" = '-' ]; then
-  set -- $HAPROXY_EXPORTER_BIN "$@"
+  set -- /bin/haproxy_exporter "$@"
 fi
 
 exec "$@"
